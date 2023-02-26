ff580bbb75088088aff37ce938b3fa9e286a75d1
12hyeon <hyeonjeong1212@naver.com>
2023-02-26 23:37:11 +0900

    [백준] 1158 요세푸스 문제 - 성공

--- BOJ/algorithm_basics/data_structure1/Josephus_problem.py
+++ BOJ/algorithm_basics/data_structure1/Josephus_problem.py
@@ -1,26 +1,14 @@
-# 시간 초과
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
-q2 = list(range(n,0,-1))
-q1 = list()
-
+num = [i for i in range(1, n+1)]
 i = 0
-std = 0
 result = []
-while len(result) < n-1:
-    if len(q2) == 0:
-        q1.reverse()
-        q1, q2 = q2, q1
-    if std == m-1:
-        result.append(q2.pop())
-        std = 0
-        #print('r : ', result)
-    else:
-        q1.append(q2.pop())
-        std += 1
-q1 = q1+q2
-print('<', end='')
-for r in result:
-    print(str(r)+',', end=' ')
-print(str(q1[0])+'>', end='')
+
+for _ in range(n):
+    i += m-1
+    if i >= len(num):
+        i %= len(num) # 삭제로 인한
+    result.append(num.pop(i))
+
+print('<'+', '.join(map(str, result))+'>')
