9b7dacbff1618156be63a0b1a57bd51dcc881dea
12hyeon <hyeonjeong1212@naver.com>
2023-03-01 21:57:24 +0900

    [백준] 17298 오큰수 - 성공

--- BOJ/algorithm_basics/data_structure1/NEG.py
+++ BOJ/algorithm_basics/data_structure1/NEG.py
@@ -1,4 +1,5 @@
 # 자기보다 큰 수 중 가장 오른쪽 수 출력
+# = 인덱스 0부터 왼쪽 인덱스 값들이 자기보다 작으면 덮게
 
 import sys
 
@@ -6,15 +7,17 @@ n = int(sys.stdin.readline())
 num = list(map(int,sys.stdin.readline().split()))
 stack = []
 
-i = 0
-for i in range(n-1):
-    cnt = i+1
-    while cnt < n:
-        if num[cnt] > n :
-            print(num[cnt], end=' ')
-            break
-        elif cnt == n-1:
-            print(-1, end=' ')
-        cnt += 1
+for i in range(n):
+    if len(stack) == 0:
+        stack.append(i) # 인덱스
+    else: # stack에는 자기보다 작은 인덱스들이 들어있음
+        while len(stack) != 0:
+            if num[i] > num[stack[-1]]:
+                num[stack.pop()] = num[i]
+            else:
+                break
+        stack.append(i)
+for s in stack:
+    num[s] = -1
 
-print(-1, end=' ')
+print(' '.join([str(n) for n in num]))
