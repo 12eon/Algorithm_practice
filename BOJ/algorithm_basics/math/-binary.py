ff3f4d10329259f985b7dff9d2c4bbffc06a2c83
12hyeon <hyeonjeong1212@naver.com>
2023-03-06 13:23:17 +0900

    [백준] 2089 -2진수(정리)

--- BOJ/algorithm_basics/math/-binary.py
+++ BOJ/algorithm_basics/math/-binary.py
@@ -4,7 +4,7 @@ n = int(sys.stdin.readline().rstrip())
 result = []
 
 while n != 0:
-    if n%-2:
+    if n%-2: # -2로 나눈 나머지 == -1
         n=n//-2+1
         result.append('1')
     else:
@@ -12,14 +12,3 @@ while n != 0:
         result.append('0')
 result.reverse()
 print(''.join(result))
-
-# 양수처리 실패
-"""if n < 0:
-    result = ['1']
-else:
-    result = []
-i = 0
-while n != 0:
-    result.append(str(n%2))
-    n //= -2
-print(''.join(result))"""
