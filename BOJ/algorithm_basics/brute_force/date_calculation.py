704778b28860f728566c991b2e95054cad126a75
12hyeon <hyeonjeong1212@naver.com>
2023-03-13 22:59:20 +0900

    [백준] 1476 날짜 계산

--- BOJ/algorithm_basics/brute_force/date_calculation.py
+++ BOJ/algorithm_basics/brute_force/date_calculation.py
@@ -1,15 +1,9 @@
 import sys
 
 n = list(map(int, sys.stdin.readline().split()))
-std = [15,28,19]
-result = 0
-
-if n[0] == n[1] and n[1] == n[2]:
-    print(n[0])
-else:
-    goal = 1
-    while True:
-        if (goal-n[0])%15 == 0 and (goal-n[1])%28 == 0 and (goal-n[2])%19 == 0:
-            break
-        goal += 1
-    print(goal)
+goal = 1
+while True:
+    if (goal-n[0])%15 == 0 and (goal-n[1])%28 == 0 and (goal-n[2])%19 == 0:
+        break
+    goal += 1
+print(goal)
