90b44ae522662cf174fc7c29c8cb1ff9d17c7a9d
12hyeon <hyeonjeong1212@naver.com>
2023-12-14 13:45:17 +0900

    [프로그래머스] 점 찍기 & 택배 배달과 수거하기

--- Programmers/Lv_2/택배_배달과_수거하기.py
+++ Programmers/Lv_2/택배_배달과_수거하기.py
@@ -1,18 +1,14 @@
-# 초기 : while로 1개씩 처리 -> 시간초과
-# 해결 : cnt가 끊어지는 부분
-
 def solution(cap, n, deliveries, pickups):
-    d_val, p_val = 0,0
+    d_val, p_val = 0, 0
     answer = 0
-    for i in range(n):
-        d_val += deliveries[n-i-1]
-        p_val += pickups[n-i-1]
-        while p_val > 0 or d_val > 0:
-        # 값이 시작되는 경우(0 -> 0 초과), cap 값을 초과한 경우 탐색(값-cap > 0)
-        # while : 해당 위치에 처리 값이 많으면, 한번 더해진 경우에 처리량 > cnt
+
+    for i in range(n):  # 누적시키면서 초과 여부 확인
+        d_val += deliveries[n - i - 1]
+        p_val += pickups[n - i - 1]
+
+        while p_val > 0 or d_val > 0:  # 작업이 있는 경우
             d_val -= cap
             p_val -= cap
-            answer += 2 * (n-i)
-            print(d_val, p_val, n-i)
-        print(d_val, p_val, n-i)
+            answer += 2 * (n - i)  # 작업의 위치
+
     return answer
