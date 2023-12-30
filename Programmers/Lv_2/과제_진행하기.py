ecbb7169a419868abcdf808052b01c626d12645e
12hyeon <hyeonjeong1212@naver.com>
2023-12-30 10:05:52 +0900

    [프로그래머스] 과제 진행하기

--- Programmers/Lv_2/과제_진행하기.py
+++ Programmers/Lv_2/과제_진행하기.py
@@ -1,33 +1,29 @@
-# i방문 : (i-1)이 최대한 처리 & 처리가 되는 경우는 answer에 담기
+# hour + minute은 분으로 통일
+# while문에서 비교 후 처리 꼼꼼히!
+
 def solution(plans):
-    time = []
-    plans.sort(key = lambda x :(x[1]))
-    for i in range(len(plans)):
-        p = plans[i]
-        time.append(int(p[2]))
-        plans[i][2] = int(p[2])
+    p = [[str(n), int(t[:2]) * 60 + int(t[3:]), int(d)] for n, t, d in plans]
+    p.sort(key=lambda x: x[1])  # 시간은 분으로
 
+    q = []
     answer = []
-    pre = [0]
-    for i in range(1, len(plans)):
-        p = plans[i]
-        n = pre[-1]
-        h2,s2 = map(int,plans[i][1].split(':'))
-        h, s = map(int,plans[n][1].split(':'))
-        rest = 60*(h2-h) + (s2-s)
-        while len(pre) > 0:
-            n = pre[-1]
-            if rest >= time[n]:
-                rest -= time[n]
-                time[n] = 0
-                answer.append(plans[n][0])
-                pre.pop()
-            else:
-                time[n] -= rest
-                rest = 0
-                break
-        pre.append(i)
-    for p in pre[-1::-1]:
-        answer.append(plans[p][0])
+    leng = len(plans)
 
+    for i in range(0, leng - 1):
+        df = p[i + 1][1] - p[i][1]
+        if (df < p[i][2]):  # 미완료
+            q.append([p[i][0], p[i][2] - df])
+        else:
+            df -= p[i][2]
+            answer.append(p[i][0])
+            while df > 0 and len(q) > 0:  # 남은 시간
+                if df >= q[-1][1]:
+                    df -= q[-1][1]  # 필요
+                    answer.append(q.pop()[0])
+                else:
+                    q[-1][-1] -= df
+                    break
+    answer.append(p[-1][0])
+    while q:
+        answer.append(q.pop()[0])
     return answer
